8429e6e800b69334598623afdae2307d6b43f1e1
Moustafa Aboulazm <96114396+Moustafa1997@users.noreply.github.com>
2025-01-14 00:29:02 +0200

    Update app.js

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ const limiter = rateLimit({
   windowMs: 1 * 60 * 1000,
   message: 'Too many request from this IP, please try again in an hour!',
 });
-app.use('/api', limiter);
+app.use('/api');
 
 // for parsing application/json from body
 app.use(express.json());
